6c1a260e939747edcbf556f95891a38196b6a5ec
SeppeV <seppe.vuerstaek@outlook.com>
2022-04-26 08:51:19 +0200

    1.3 werkend

--- 1.3.py
+++ 1.3.py
@@ -1,25 +1,31 @@
-import RPi.GPIO as GPIO # Only works on Raspberry Pi
-import time
+import RPi.GPIO as GPIO  # Only works on Raspberry Pi
+import time 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
+class Led:
+    def __init__(self, pin):
+        self.pin = pin
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
+
+led_1 = Led(36)
+led_2 = Led(38)
+led_3 = Led(31)
+led_4 = Led(33)
 
 while True:
-    print("Even on, Odd off")
-    GPIO.output(16,GPIO.HIGH)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(12 ,GPIO.HIGH)
-    GPIO.output(5 ,GPIO.LOW)
-
     time.sleep(1)
-
-    print("Even off, Odd on")
-    GPIO.output(16,GPIO.LOW)
-    GPIO.output(13,GPIO.HIGH)
-    GPIO.output(12 ,GPIO.LOW)
-    GPIO.output(5 ,GPIO.HIGH)
+    led_1.on()
+    led_2.on()
+    led_3.off()
+    led_4.off()
+    time.sleep(1)
+    led_1.off()
+    led_2.off()
+    led_3.on()
+    led_4.on()
